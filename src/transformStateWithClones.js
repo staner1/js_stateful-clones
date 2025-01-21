9b05703bb58b39cc341d982c2a01d5b58c0bf75a
Oleksandr Kozyriev <kozyrev.sania@gmail.com>
2025-01-21 23:21:36 +0200

    task completed

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateCopy = { ...state };
+  const historyChanges = [];
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    if (type === 'addProperties') {
+      for (const key in extraData) {
+        stateCopy[key] = extraData[key];
+      }
+    }
+
+    if (type === 'removeProperties') {
+      for (const key of keysToRemove) {
+        delete stateCopy[key];
+      }
+    }
+
+    if (type === 'clear') {
+      for (const key in stateCopy) {
+        delete stateCopy[key];
+      }
+    }
+
+    historyChanges.push(Object.assign({}, stateCopy));
+  }
+
+  return historyChanges;
 }
 
 module.exports = transformStateWithClones;
